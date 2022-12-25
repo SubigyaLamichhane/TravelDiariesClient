05725fc4620bc433efd7351621a73e68e43eeae2
subigyalamichhane <subigyalamichhane@gmail.com>
2022-12-25 08:20:02 +0545

    never

--- src/api/api.js
+++ src/api/api.js
@@ -1,70 +1,62 @@
-import axios from "axios"
+import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
+export const API_URL = process.env.API_URL || 'http://localhost:3001';
 
+export const gettAllPosts = async () => {
+  const data = await axios.get(API_URL + '/posts');
 
+  return data.data;
+};
 
-export const gettAllPosts = async ()=>{
-  
-    const data = await axios.get("http://localhost:3000/posts")
+export const getUser = async (id) => {
+  const user = await axios.get(`${API_URL}/user/${id}`);
 
-    return data.data
-}
+  return user.data;
+};
 
-export const getUser= async (id)=>{
-  
-    const user = await axios.get(` http://localhost:3001/user/${id}`)
-
-    return user.data
-}
-
-export const sendAuthReq = async(signup,data)=>{
-
-    const Data = await axios.post(`http://localhost:3001/user/${signup?"signup":"login"}`,
-    {
-        name:data.name?data.name:null,
-        image:data.image?data.image:null,
-        email:data.email,
-        password:data.password
-    }).catch((err)=>console.log(err))
-    const userData = await Data.data
-    return userData 
-}
-
-export const addPost = async (postData)=>{
-    const data = await axios.post('http://localhost:3001/posts/add_post',{
-        title:postData.title,
-        description:postData.description,
-        location:postData.location,
-        image:postData.image,
-        user:postData.user
+export const sendAuthReq = async (signup, data) => {
+  const Data = await axios
+    .post(`${API_URL}/user/${signup ? 'signup' : 'login'}`, {
+      name: data.name ? data.name : null,
+      image: data.image ? data.image : null,
+      email: data.email,
+      password: data.password,
     })
-    return data.data
-}
+    .catch((err) => console.log(err));
+  const userData = await Data.data;
+  return userData;
+};
 
-export const deletePostByID = async (id)=>{
- 
-  const data = await axios.delete(` http://localhost:3001/posts/${id}`)
-  return data
- 
-}
+export const addPost = async (postData) => {
+  const data = await axios.post(`${API_URL}/posts/add_post`, {
+    title: postData.title,
+    description: postData.description,
+    location: postData.location,
+    image: postData.image,
+    user: postData.user,
+  });
+  return data.data;
+};
 
-export const getPostById = (post_id,user_id)=>{
-    const post = axios.get(`http://localhost:3001/posts/${post_id}`,{
-        user:user_id
-    })
-    return post
-}
+export const deletePostByID = async (id) => {
+  const data = await axios.delete(`${API_URL}/posts/${id}`);
+  return data;
+};
 
+export const getPostById = (post_id, user_id) => {
+  const post = axios.get(`${API_URL}/posts/${post_id}`, {
+    user: user_id,
+  });
+  return post;
+};
 
+export const editPost = async (id, data) => {
+  const post = await axios.patch(`${API_URL}/posts/${id}/edit`, {
+    title: data.title,
+    description: data.description,
+    location: data.location,
+  });
 
-
-export const editPost = async (id,data)=>{
-    const post = await axios.patch(`http://localhost:3001/posts/${id}/edit`,{
-        title:data.title,
-        description:data.description,
-        location:data.location
-        })
-
-    return post.data
-} 
+  return post.data;
+};
