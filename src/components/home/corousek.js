9a5ccd4117181876052bea799879a28490533334
subigyalamichhane <subigyalamichhane@gmail.com>
2022-12-25 08:41:00 +0545

    image cover

--- src/components/home/corousek.js
+++ src/components/home/corousek.js
@@ -2,32 +2,31 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function CarouselComp() {
   return (
-    <Carousel className='carousel'>
+    <Carousel className="carousel">
       <Carousel.Item>
         <img
+          style={{ objectFit: 'cover' }}
           className="d-block carousel"
           src="https://source.unsplash.com/random"
           alt="First slide"
         />
-        
       </Carousel.Item>
       <Carousel.Item>
         <img
+          style={{ objectFit: 'cover' }}
           className="d-block carousel"
           src="https://picsum.photos/200/300"
           alt="First slide"
         />
-        
       </Carousel.Item>
       <Carousel.Item>
         <img
+          style={{ objectFit: 'cover' }}
           className="d-block carousel"
           src="https://picsum.photos/200/"
           alt="First slide"
         />
-        
       </Carousel.Item>
-     
     </Carousel>
   );
 }
